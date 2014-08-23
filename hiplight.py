336347452b5c277040eaf0959c162c691b8e9c46
Macklin Underdown <macklin@detroitlabs.com>
2014-08-23 18:43:01 -0400

    start breaking out code into classes

--- hiplight.py
+++ hiplight.py
@@ -2,62 +2,47 @@ import os
 from pprint import PrettyPrinter
 import requests
 import RPi.GPIO as GPIO
+from status import Status
 
+pp = PrettyPrinter(indent=2)
+red = 11
+blue = 13
+green = 15
 
-def main():
-    status = {
-        True: {
-            'status': 'available',
-            'color': 'green'
-        },
-        'xa': {
-            'status': 'away',
-            'color': 'yellow'
-        },
-        'dnd': {
-            'status': 'do_not_disturb',
-            'color': 'red'
-        },
-        False: {
-            'status': 'offline',
-            'color': None
-        }
-    }
+def make_request():
+  params = {'auth_token': os.environ.get('HIPCHAT_API_TOKEN')}
+  return requests.get(url=("https://api.hipchat.com/v2/user/%s" % os.environ.get('HIPCHAT_EMAIL_ADDRESS')), params=params).json()
 
-    pp = PrettyPrinter(indent=2)
+def setup_pins():
+  GPIO.setmode(GPIO.BOARD)
 
-    params = {'auth_token': os.environ.get('HIPCHAT_API_TOKEN')}
-    r = requests.get(url=("https://api.hipchat.com/v2/user/%s" % os.environ.get('HIPCHAT_EMAIL_ADDRESS')), params=params).json()
+  GPIO.setup(red, GPIO.OUT) #setup all the pins
+  GPIO.setup(green, GPIO.OUT)
+  GPIO.setup(blue, GPIO.OUT)
 
-    current = None
-    if r['presence']:
-        presence = r['presence']
-        try:
-            current = status[presence['show']]
-        except KeyError:
-            current = status[presence['is_online']]
+  Freq = 100 #Hz
 
-        try:
-            current['message'] = presence['status']
-        except KeyError:
-            current['message'] = None
-    else:
-        current = status[False]
+  #setup all the colours
+  RED = GPIO.PWM(red, Freq) #Pin, frequency
+  RED.start(0) #Initial duty cycle of 0, so off
+  GREEN = GPIO.PWM(green, Freq)
+  GREEN.start(0)
+  BLUE = GPIO.PWM(blue, Freq)
+  BLUE.start(0)
 
-    pp.pprint(current)
-
-    try:
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(11, GPIO.OUT)
-        GPIO.output(11, 1)
-        GPIO.setup(13, GPIO.OUT)
-        GPIO.output(13, 0)
-        GPIO.setup(15, GPIO.OUT)
-        GPIO.output(15, 1)
-    except KeyboardInterrupt:
-        GPIO.cleanup()
+def teardown():
+  GPIO.cleanup()
 
 
 
 if __name__ == '__main__':
-    main()
+    status = Status()
+    setup_pins()
+    response = make_request()
+    status.determine_status(response)
+    print status.get_availability()
+    try:
+        while True:
+            i = 0
+    except KeyboardInterrupt:
+        teardown()
---
+++ status.py
@@ -0,0 +1,39 @@
+class Status:
+    def __init__(self):
+        self.map = {
+            True: {
+                'status': 'available',
+                'color': 'green'
+            },
+            'xa': {
+                'status': 'away',
+                'color': 'yellow'
+            },
+            'dnd': {
+                'status': 'do_not_disturb',
+                'color': 'red'
+            },
+            False: {
+                'status': 'offline',
+                'color': None
+            }
+        }
+        self.current = None
+
+    def determine_status(self, response):
+        if response['presence']:
+            presence = response['presence']
+            try:
+                self.current = self.map[presence['show']]
+            except KeyError:
+                self.current = self.map[presence['is_online']]
+
+            try:
+                self.current['message'] = self.map['status']
+            except KeyError:
+                self.current['message'] = None
+        else:
+            self.current = self.map[False]
+
+    def get_availability(self):
+        return self.current['status']
