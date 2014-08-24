7bebd6e9e3c0e9f422da16271982195a9a02fccf
Macklin Underdown <macklin@detroitlabs.com>
2014-08-24 13:02:05 -0400

    input script

--- hiplight.py
+++ hiplight.py
@@ -1,16 +1,24 @@
 from pins import Pins
 from hipchat import HipChat
 
+def scale(value):
+    return (((value - 0) * (0 - 100)) / (255 - 0)) + 100
+
 if __name__ == '__main__':
     pins = Pins()
     hipchat = HipChat()
 
     pins.setup()
-    status = hipchat.get_user_status()
-    pins.set_color(status.get_color())
+    # status = hipchat.get_user_status()
+    # pins.set_color(status.get_color())
 
     try:
         while True:
-            i = 0
+            response = raw_input("RBG: ").split()
+            floats = map(float, response)
+            colors = map(scale, floats)
+            print colors
+            if len(colors) == 3:
+                pins.set_color(colors)
     except KeyboardInterrupt:
         pins.teardown()
